110e8b82514d4ff191e8eeebaa9c53ff7b061302
Anna Vo <avo1@ufl.edu>
2024-10-23 20:53:35 -0400

    added the decode function

--- Lab 6.py
+++ Lab 6.py
@@ -16,6 +16,16 @@ def encode(string):
     return ''.join(encoded_string)
     
 #Decoder here
+def decode(encoded_pass):
+    decoded_pass = ""
+    for digit in encoded_pass:
+        original_digit = int(digit) - 3
+        # If the new digit is less than 0, add 10 to the digit
+        if original_digit < 0:
+            original_digit += 10
+        decoded_pass += str(original_digit)
+    return decoded_pass
+
         
 if __name__ == "__main__":
     cont = True
@@ -25,7 +35,7 @@ if __name__ == "__main__":
             password = input ("Please enter your password to encode: ")
             print("Your password has been encoded and stored!\n")
         elif choice == 2:
-            print(f"The encoded password is {encode(password)}, and the original password is {password}.\n")
+            print(f"The encoded password is {encode(password)}, and the original password is {decode(encode(password))}.\n")
         elif choice == 3:
             cont = False
     
